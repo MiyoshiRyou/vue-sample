73a675d1a3c7ba9bc1a1b398157637768a67305c
MiyoshiRyou <code34434@gmail.com>
2023-04-06 10:08:13 +0900

    provide / inject

---
+++ vue9/src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { inject } from 'vue';
+import { todoKey } from '../useTodo';
+
+const state = inject(todoKey)
+if (!state) {
+  throw new Error('state is undefined')
+}
+
+const { todos, addTodo: _addTodo } = state
+
+// const todos = inject('todos')
+// const _addTodo = inject('addTodo')
+
+const addTodo = (title: string) => {
+  _addTodo(title)
+}
+
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li :style="{ backgroundColor: 'aliceblue', marginBottom: '10px' }" v-for="todo in todos">{{ todo.title }}</li>
+    </ul>
+    <button @click="addTodo('add!')">Add</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
